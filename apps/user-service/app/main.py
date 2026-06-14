4ee0ba215aeee16429de0fd71fdcf07fe111ed61
Haikel <shegatube390@gmail.com>
2026-06-14 13:46:22 +0000

    Add Prometheus metrics endpoint

--- apps/user-service/app/main.py
+++ apps/user-service/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 import os
 
 app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
 
 VERSION = os.getenv("APP_VERSION", "v1")
 
